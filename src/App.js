04bb9dd148905ad48caa0ed5c231c7c80c76ba22
chrisaydat <chrisaydat@gmail.com>
2024-05-24 15:21:21 +0000

    verge init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
 import './App.css';
+import Header from './components/header';
+import HeroSection from './components/HeroSection';
+import Features from './components/Features';
+// import About from './components/About';
+// import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <HeroSection />
+      <Features />
+      {/* <About />
+      <Footer /> */}
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+// src/components/Header.js
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <h1>Verge, Inc.</h1>
+        <ul>
+          <li><a href="#hero">Home</a></li>
+          <li><a href="#features">Features</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#footer">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
