a910a56288945b3ba882a0bf9e7534afaff31274
chrisaydat <chrisaydat@gmail.com>
2024-05-25 22:44:53 +0000

    header stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-// src/App.js
 import React from 'react';
 import './App.css';
 import Header from './components/header';
 import HeroSection from './components/HeroSection';
 import Features from './components/Features';
-// import About from './components/About';
-// import Footer from './components/Footer';
+
 
 function App() {
   return (
@@ -13,8 +11,6 @@ function App() {
       <Header />
       <HeroSection />
       <Features />
-      {/* <About />
-      <Footer /> */}
     </div>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,18 @@
-// src/components/Header.js
 import React from 'react';
+import './Header.css';
 
-const Header = () => {
+function Header() {
   return (
-    <header>
-      <nav>
-        <h1>Verge, Inc.</h1>
-        <ul>
-          <li><a href="#hero">Home</a></li>
-          <li><a href="#features">Features</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#footer">Contact</a></li>
-        </ul>
-      </nav>
-    </header>
+    <nav className="custom-navbar">
+      <ul className="nav-links">
+        <li className="nav-item">Relay</li>
+        <li className="nav-item">Company</li>
+        <li className="nav-item">FAQs</li>
+        <li className="nav-item">Blog</li>
+        <li className="nav-item">Contact</li>
+      </ul>
+    </nav>
   );
-};
+}
 
 export default Header;
