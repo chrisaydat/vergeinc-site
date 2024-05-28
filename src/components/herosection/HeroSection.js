038f2246d8bd79fc6454105213d58336eab1c0e7
Chris Festus Otopa Ayeh-Datey <chrisaydat@gmail.com>
2024-05-28 18:22:00 +0000

    hero section text stuff

--- src/components/herosection/HeroSection.js
+++ src/components/herosection/HeroSection.js
@@ -8,7 +8,7 @@ const Hero = () => {
       <div className="hero-background"></div>
       <div className="hero-content">
         <h1>Verge</h1>
-        <p>I am not an expert and i do not need to be.</p>
+        <p>You may not be an expert and you do not need to be.</p>
         <div className="hero-buttons">
         <a href="https://www.linkedin.com/company/verge-fintech/" className="hero-button">Learn more</a>
         <a href="https://2c06dnupxht.typeform.com/to/ZJdIB2Gz" className="hero-button">Join Waitlist</a>
